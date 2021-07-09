8bb1d42800521226c479e5fed88e071ffd1fe3c3
Mesrop <mesrop@iMac-3.local>
2021-07-09 13:44:16 +0400

    1

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import { ValidationSchemaExample } from "./components/Form";
+
+function App() {
+  return <ValidationSchemaExample />;
+}
+
+export default App;
---
+++ src/components/Form.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import "./style.css";
+
+const SignupSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  lastName: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+});
+
+export const ValidationSchemaExample = () => (
+  <div>
+    <h1>Signup</h1>
+    <Formik
+      initialValues={{
+        firstName: "",
+        lastName: "",
+        email: "",
+      }}
+      validationSchema={SignupSchema}
+      onSubmit={(values) => {
+        // same shape as initial values
+        console.log(values);
+      }}
+    >
+      {({ errors, touched }) => (
+        <Form className="form">
+          <Field
+            name="firstName"
+            className="inputes"
+            placeholder="First Name"
+          />
+          {errors.firstName && touched.firstName ? (
+            <div>{errors.firstName}</div>
+          ) : null}
+          <Field name="lastName" className="inputes" placeholder="Last Name" />
+          {errors.lastName && touched.lastName ? (
+            <div>{errors.lastName}</div>
+          ) : null}
+          <Field
+            name="email"
+            type="email"
+            className="inputes"
+            placeholder="Email"
+          />
+          {errors.email && touched.email ? <div>{errors.email}</div> : null}
+          <button type="submit" className="button">
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
